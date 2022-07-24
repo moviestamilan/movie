ec43786f54c61af54b059f9fa41b65307709c2f5
moviestamilan <109806986+moviestamilan@users.noreply.github.com>
2022-07-24 10:20:55 +0530

    Update babel1.js

--- babel1.js
+++ babel1.js
@@ -31,10 +31,45 @@ const First = (props) =>{
 const App = () => {
   return(
      <span>
+          <First src='img/thegreyman.jpg'
+          title='The Gray Man (2022) HD 720p Tamil Dubbed Movie Watch Online'  
+          href='http://vidmx.xyz/zembed-resbvsnuti6l.html'
+ 
+           />
+
+ <First src='img/funandfrustration.jpg'
+          title='F3: Fun and Frustration (2022) HD 720p Tamil Movie Watch Online'  
+          href='http://vidmx.xyz/xembed-91vmp3zajwyb.html'
+ 
+           />
+
+     <First src='img/iravinnizhal.jpg'
+          title='Iravin Nizhal (2022) HQ DVDScr Tamil Full Movie Watch Online'  
+          href='http://vidmx.xyz/embed-8a0d3wqis0oz.html'
+ 
+           />
+<First src='img/thewarrior.jpg'
+          title='The Warriorr (2022) HQ DVDScr Tamil Full Movie Watch Online'  
+          href='http://vidmx.xyz/embed-s9dma2jlme5f.html'
+ 
+           />
+
+
+     <First src='img/mydearbootham.jpg'
+          title='My Dear Bootham (2022) HQ DVDScr Tamil Full Movie Watch Online'  
+          href='http://vidmx.xyz/embed-sti2z585l1l2.html'
+ 
+           />
          <First src='img/maamanithan.jpg'
           title='	maamanithan (2022) tamil movies  HD 720 watch online'  
           href='http://vidmx.xyz/yembed-og4omgms0bnh.html'
  
+           />
+
+             <First src='img/gargi.jpg'
+          title='Gargi (2022) HQ DVDScr Tamil Full Movie Watch Online'  
+          href='http://vidmx.xyz/embed-4owvh9go1ldx.html'
+         
            />
            <First src='img/bestie.jpg'
           title=' bestie (2022) tamil movies  HD 720 watch online'  
@@ -46,6 +81,18 @@ const App = () => {
           href='http://vidmx.xyz/embed-kf186sgcb7cy.html'
  
            />
+                <First src='img/adedesundara.jpg'
+          title='Adade Sundara (2022) HD 720p Tamil Movie Watch Online'  
+          href='http://vidmx.xyz/embed-kw86in2fgirg.html'
+ 
+           />
+                   <First src='img/pannikutty.jpg'
+          title='Panni Kutty (2022) HQ DVDScr Tamil Full Movie Watch Online'  
+          href='http://vidmx.xyz/embed-j99q9pxne8d7.html'
+ 
+           />
+           
+           
      <First src='img/vikram.jpeg'
           title=' vikram (2022) tamil movies  HD 720 watch online'  
           href='http://vidmx.xyz/embed-62g253t7zk39.html'
@@ -56,6 +103,7 @@ const App = () => {
           href='http://vidmx.xyz/embed-ej8zf2si0i95.html'
          
            />
+
 
 
     </span>
